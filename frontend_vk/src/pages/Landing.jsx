667d16c4bb03eceaf9700081208ae589f9ae97de
avadhesh11 <202452309@iiitvadodara.ac.in>
2026-02-08 00:30:19 +0530

    landing page changed

--- frontend_vk/src/pages/Landing.jsx
+++ frontend_vk/src/pages/Landing.jsx
@@ -1,7 +1,19 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-
+import { Link, useNavigate, useLocation } from 'react-router-dom';
+import {useAuth} from "../context/AuthContext"
 const Landing = () => {
+  const navigate=useNavigate();
+  const { user, loading } = useAuth();
+  if(user){
+    navigate("/dashboard");
+  }
+   if (loading) {
+    return (
+      <div className="flex items-center justify-center h-screen">
+        <div className="animate-spin rounded-full h-12 w-12 border-b-2 border-blue-500"></div>
+      </div>
+    );
+  }
   return (
     <div className="min-h-screen flex flex-col items-center justify-center bg-gray-50">
       <h1 className="text-4xl font-bold mb-8 text-primary">StudySphere</h1>
